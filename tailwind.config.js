a859b33b3590d75971e34063dd8ac40ce714187e
Bartosz Sarnowski <bartosz.sarnowski@o2.pl>
2024-08-15 19:35:36 +0200

    Added colors palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 export default {
 	content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				'primary-dark': '#282C3D',
+				primary: '#282C3DF2',
+				'primary-light': '#282C3DE0',
+				secondary: '#0085BE',
+				'secondary-light': '#00A4EA',
+				'gray-light': '#B9A9A',
+			},
+		},
 	},
 	plugins: [],
 };
