550c232d38d4b1f7999dc71df56b1e7affcc3b6f
Bartosz Sarnowski <bartosz.sarnowski@o2.pl>
2024-08-15 21:53:22 +0200

    Added links to TopNavbar component

--- src/components/navigation/top/TopNavbar.jsx
+++ src/components/navigation/top/TopNavbar.jsx
@@ -4,7 +4,13 @@ export default function TopNavbar() {
 	return (
 		<nav>
 			<ul>
-				<NavLink>Home</NavLink>
+				<NavLink>Live</NavLink>
+				<NavLink>Favorite</NavLink>
+				<NavLink>My Bets</NavLink>
+			</ul>
+			<ul>
+				<NavLink>Deposit</NavLink>
+				<NavLink>$2568.19</NavLink>
 			</ul>
 		</nav>
 	);
