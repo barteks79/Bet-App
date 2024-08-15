4a043f586db56c3a55606e3a517500ab739e108f
Bartosz Sarnowski <bartosz.sarnowski@o2.pl>
2024-08-15 21:30:25 +0200

    Created RootLayout, Aside & Top Navbars

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,15 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import RootLayout from './pages/RootLayout';
+
 function App() {
-	return;
+	const router = createBrowserRouter([
+		{
+			path: '/',
+			element: <RootLayout />,
+		},
+	]);
+
+	return <RouterProvider router={router} />;
 }
 
 export default App;
