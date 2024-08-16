4909626daea61da1c6b728940e3e87105798c89c
Bartosz Sarnowski <bartosz.sarnowski@o2.pl>
2024-08-16 22:19:29 +0200

    Changed some details in AsideDetails component

--- src/components/navigation/aside/AsideCategory.jsx
+++ src/components/navigation/aside/AsideCategory.jsx
@@ -30,7 +30,7 @@ function AsideCategory({ icon, children }) {
 				</p>
 				<FontAwesomeIcon icon={faChevronUp} rotation={isCategoryOpen ? 180 : 0} className={styles.chevron} />
 			</button>
-			<AsideDetails isOpen={isCategoryOpen} type={category} />
+			<AsideDetails isOpen={isCategoryOpen} />
 		</li>
 	);
 }
--- src/components/navigation/aside/AsideDetails.jsx
+++ src/components/navigation/aside/AsideDetails.jsx
@@ -1,13 +1,17 @@
 /* eslint-disable react/prop-types */
+import { useSelector } from 'react-redux';
 import { NavLink } from 'react-router-dom';
 import styles from './Animations.module.css';
 
-function AsideDetails({ isOpen, type }) {
+function AsideDetails({ isOpen }) {
+	const category = useSelector(state => state.ui.openDropdown);
+
 	return (
-		<ul className={`w-full bg-primary text-xl ${styles.dropdown} ${isOpen && styles.open}`}>
+		// add dynamic max-height depending on data length
+		<ul className={`flex flex-col gap-5 w-full bg-primary text-xl ${isOpen && 'py-5 max-h-20'} ${styles.dropdown}`}>
 			<li className="flex w-aside-wrapper justify-center items-center gap-5">
 				<img src="https://media.api-sports.io/football/leagues/88.png" className="w-10" />
-				<NavLink>{type}</NavLink>
+				<NavLink>{category}</NavLink>
 			</li>
 		</ul>
 	);
